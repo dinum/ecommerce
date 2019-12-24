decf310186bda80163c24e2f50129c59b3d3528c
Dinum Dissanayaka - Home <m.dinum@yahoo.com>
2019-12-24 06:12:03 +0530

    sql

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 22, 2019 at 08:16 PM
+-- Generation Time: Dec 24, 2019 at 12:40 AM
 -- Server version: 5.7.26
 -- PHP Version: 5.6.40
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `permission_roles` (
   PRIMARY KEY (`id`),
   KEY `role_idx` (`role_id`),
   KEY `permission_idx` (`permission_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `permission_roles`
@@ -54,7 +54,10 @@ INSERT INTO `permission_roles` (`id`, `role_id`, `permission_id`) VALUES
 (7, 2, 3),
 (8, 2, 4),
 (9, 3, 7),
-(10, 3, 8);
+(10, 3, 8),
+(11, 4, 9),
+(12, 4, 10),
+(13, 4, 11);
 
 -- --------------------------------------------------------
 
@@ -73,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `tbllog` (
   `values` text NOT NULL,
   `ip_add` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=63 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=87 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbllog`
@@ -141,7 +144,31 @@ INSERT INTO `tbllog` (`id`, `log_date`, `log_time`, `user_name`, `user_id`, `des
 (59, '2019-12-22', '19:28:31', 'Dinum', 0, 'User LogOut -  / USER - Dinum ID - 3 / Date 2019-12-22 19:28:31', '[]', '127.0.0.1'),
 (60, '2019-12-22', '19:28:38', 'dinum2', 0, 'User Login -  / USER - dinum2 ID - 3 / Date 2019-12-22 19:28:38', '[]', '127.0.0.1'),
 (61, '2019-12-22', '19:35:26', 'Dinum', 0, 'User LogOut -  / USER - Dinum ID - 3 / Date 2019-12-22 19:35:26', '[]', '127.0.0.1'),
-(62, '2019-12-22', '19:35:33', 'admin', 0, 'User Login -  / USER - admin ID - 1 / Date 2019-12-22 19:35:33', '[]', '127.0.0.1');
+(62, '2019-12-22', '19:35:33', 'admin', 0, 'User Login -  / USER - admin ID - 1 / Date 2019-12-22 19:35:33', '[]', '127.0.0.1'),
+(63, '2019-12-22', '20:23:02', 'Administrator', 0, 'User LogOut -  / USER - Administrator ID - 1 / Date 2019-12-22 20:23:02', '[]', '::1'),
+(64, '2019-12-22', '20:23:16', 'dinum2', 0, 'User Login -  / USER - dinum2 ID - 3 / Date 2019-12-22 20:23:16', '[]', '::1'),
+(65, '2019-12-23', '14:29:58', 'admin', 0, 'User Login -  / USER - admin ID - 1 / Date 2019-12-23 14:29:58', '[]', '::1'),
+(66, '2019-12-23', '14:30:06', 'Administrator', 0, 'User LogOut -  / USER - Administrator ID - 1 / Date 2019-12-23 14:30:06', '[]', '::1'),
+(67, '2019-12-23', '14:30:12', 'dinum2', 0, 'User Login -  / USER - dinum2 ID - 3 / Date 2019-12-23 14:30:12', '[]', '::1'),
+(68, '2019-12-23', '15:32:26', 'Dinum', 0, 'User LogOut -  / USER - Dinum ID - 3 / Date 2019-12-23 15:32:26', '[]', '::1'),
+(69, '2019-12-23', '15:32:34', 'admin', 0, 'User Login -  / USER - admin ID - 1 / Date 2019-12-23 15:32:34', '[]', '::1'),
+(70, '2019-12-23', '15:33:18', 'Administrator', 1, 'Add Permission | Success | ID=9', '[\"add_vacancy\",\"1\",\"\"]', '::1'),
+(71, '2019-12-23', '15:33:37', 'Administrator', 1, 'Add Permission | Success | ID=10', '[\"view_vacancy\",\"\"]', '::1'),
+(72, '2019-12-23', '15:33:54', 'Administrator', 1, 'Add Permission | Success | ID=11', '[\"vacancies_requests\",\"\"]', '::1'),
+(73, '2019-12-23', '15:58:17', 'Dinum', 1, 'Updated User | Success | ID=1', '[\"1\",\"Dinum\",\"Dissnayaka\",\"Mahawela\",\"dinum@gmail.com\",\"770307283\",\"325\\/3 gona\",\"dgsgsdg\",\"Update\"]', '::1'),
+(74, '2019-12-23', '15:58:36', 'Dinum', 1, 'Updated User | Success | ID=1', '[\"1\",\"Dinum\",\"Dissnayaka\",\"Mahawela\",\"m.dinum@gmail.com\",\"770307283\",\"325\\/3 gona\",\"dgsgsdg\",\"Update\"]', '::1'),
+(75, '2019-12-23', '16:12:17', 'Administrator', 1, 'Add Role | Success | ID=4', '[\"Manager\",[\"9\",\"10\",\"11\"],\"\"]', '::1'),
+(76, '2019-12-23', '16:13:04', 'Administrator', 1, 'Add User | Success | ID=5', '[\"manager\",\"manager@gmail.com\",\"1\",\"manager\",\"Dinum@inova123\",\"Dinum@inova123\",[\"4\"],\"\"]', '::1'),
+(77, '2019-12-23', '16:13:07', 'Administrator', 0, 'User LogOut -  / USER - Administrator ID - 1 / Date 2019-12-23 16:13:07', '[]', '::1'),
+(78, '2019-12-23', '16:13:17', 'manager', 0, 'User Login -  / USER - manager ID - 5 / Date 2019-12-23 16:13:17', '[]', '::1'),
+(79, '2019-12-23', '18:17:55', 'Guest', 0, 'Add Vacancy | title validation fail', '[\"testing data1\",\"vxdgs\",\"1\",\"12\\/23\\/2019 11:47 PM\",\"12\\/25\\/2019 11:47 PM\",\"sdfafasf\",\"2\",\"3453\",\"dfghdfhdf\",\"Save\"]', '::1'),
+(80, '2019-12-23', '18:18:31', 'Guest', 0, 'Add Vacancy | title validation fail', '[\"testing data1\",\"vxdgs\",\"1\",\"12\\/23\\/2019 11:47 PM\",\"12\\/25\\/2019 11:47 PM\",\"sdfafasf\",\"2\",\"3453\",\"dfghdfhdf\",\"Save\"]', '::1'),
+(81, '2019-12-23', '18:18:58', 'Guest', 0, 'Add Vacancy | title validation fail', '[\"testing data\",\"vxdgs\",\"1\",\"12\\/23\\/2019 11:48 PM\",\"12\\/26\\/2019 11:48 PM\",\"gretgyer\",\"2\",\"36346\",\"dfghdfhdf\",\"Save\"]', '::1'),
+(82, '2019-12-23', '18:23:51', 'Guest', 0, 'Add Vacancy | title validation fail', '[\"testing data\",\"vxdgs\",\"1\",\"12\\/23\\/2019 11:48 PM\",\"12\\/26\\/2019 11:48 PM\",\"gretgyer\",\"2\",\"36346\",\"dfghdfhdf\",\"Save\"]', '::1'),
+(83, '2019-12-23', '18:24:45', 'Guest', 0, 'Add Vacancy | title validation fail', '[\"testing data\",\"vxdgs\",\"1\",\"12\\/23\\/2019 11:48 PM\",\"12\\/26\\/2019 11:48 PM\",\"gretgyer\",\"2\",\"36346\",\"dfghdfhdf\",\"Save\"]', '::1'),
+(84, '2019-12-23', '18:25:27', 'manager', 5, 'Add Vacancy | Success | ID=1', '[\"testing data\",\"vxdgs\",\"1\",\"12\\/23\\/2019 11:48 PM\",\"12\\/26\\/2019 11:48 PM\",\"gretgyer\",\"2\",\"36346\",\"dfghdfhdf\",\"Save\"]', '::1'),
+(85, '2019-12-23', '18:31:49', 'manager', 5, 'Add Vacancy | Success | ID=2', '[\"testing data 2\",\"sdgfsdgsdg\",\"2\",\"12\\/24\\/2019 12:01 AM\",\"12\\/27\\/2019 12:01 AM\",\"sdgsdhgsdhgsdhg\",\"2\",\"463463\",\"dfhdfhdf\",\"Save\"]', '::1'),
+(86, '2019-12-23', '18:34:01', 'manager', 5, 'Add Vacancy | Success | ID=3', '[\"testing data 3\",\"dsgsdgsdgsdg\",\"2\",\"12\\/24\\/2019 12:03 AM\",\"12\\/28\\/2019 12:03 AM\",\"sdgsdgsdg\",\"2\",\"3456346\",\"sdgbsdgds\",\"Save\"]', '::1');
 
 -- --------------------------------------------------------
 
@@ -156,7 +183,7 @@ CREATE TABLE IF NOT EXISTS `tblpermission` (
   `created_date` datetime NOT NULL,
   `special_permission` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblpermission`
@@ -170,7 +197,10 @@ INSERT INTO `tblpermission` (`id`, `permission`, `created_date`, `special_permis
 (5, 'add_user_role', '2019-12-22 15:05:19', 1),
 (6, 'view_user_roles', '2019-12-22 15:05:27', 0),
 (7, 'staff_requests', '2019-12-22 19:16:12', 0),
-(8, 'view_staff', '2019-12-22 19:16:22', 0);
+(8, 'view_staff', '2019-12-22 19:16:22', 0),
+(9, 'add_vacancy', '2019-12-23 15:33:17', 1),
+(10, 'view_vacancy', '2019-12-23 15:33:37', 0),
+(11, 'vacancies_requests', '2019-12-23 15:33:54', 0);
 
 -- --------------------------------------------------------
 
@@ -185,7 +215,7 @@ CREATE TABLE IF NOT EXISTS `tblroles` (
   `description` text,
   `added_date` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblroles`
@@ -194,7 +224,8 @@ CREATE TABLE IF NOT EXISTS `tblroles` (
 INSERT INTO `tblroles` (`id`, `name`, `description`, `added_date`) VALUES
 (1, 'Administrator', NULL, '2019-12-22'),
 (2, 'Guest', NULL, '2019-12-22'),
-(3, 'HR', NULL, '2019-12-22');
+(3, 'HR', NULL, '2019-12-22'),
+(4, 'Manager', NULL, '2019-12-23');
 
 -- --------------------------------------------------------
 
@@ -214,7 +245,7 @@ CREATE TABLE IF NOT EXISTS `tblusers` (
   `added_date` date NOT NULL,
   `status` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblusers`
@@ -222,9 +253,10 @@ CREATE TABLE IF NOT EXISTS `tblusers` (
 
 INSERT INTO `tblusers` (`id`, `detail_id`, `name`, `description`, `email`, `username`, `password`, `added_date`, `status`) VALUES
 (1, NULL, 'Administrator', NULL, 'admin@gmail.com', 'admin', '202cb962ac59075b964b07152d234b70', '2019-12-14', 1),
-(2, 1, 'Dinum', NULL, 'dinum@gmail.com', 'dinum', '887bc63001cae9e14130628f2bf8b21a', '2019-12-22', 0),
+(2, 1, 'Dinum', NULL, 'm.dinum@gmail.com', 'dinum', '887bc63001cae9e14130628f2bf8b21a', '2019-12-22', 1),
 (3, 2, 'Dinum', NULL, 'dinum@gmail.com', 'dinum2', '887bc63001cae9e14130628f2bf8b21a', '2019-12-22', 1),
-(4, 3, 'Dinum', NULL, 'dinum@gmail.com', 'dinum3', '887bc63001cae9e14130628f2bf8b21a', '2019-12-22', 1);
+(4, 3, 'Dinum', NULL, 'dinum@gmail.com', 'dinum3', '887bc63001cae9e14130628f2bf8b21a', '2019-12-22', 1),
+(5, NULL, 'manager', NULL, 'manager@gmail.com', 'manager', '887bc63001cae9e14130628f2bf8b21a', '2019-12-23', 1);
 
 -- --------------------------------------------------------
 
@@ -253,9 +285,55 @@ CREATE TABLE IF NOT EXISTS `tbl_user_details` (
 --
 
 INSERT INTO `tbl_user_details` (`id`, `fname`, `mname`, `lname`, `email`, `mobile`, `address`, `qualification`, `status`, `added_date`, `updated_date`) VALUES
-(1, 'Dinum', 'Dissnayaka', 'Mahawela', 'dinum@gmail.com', 770307283, 'fsaffasfasfasf', 'dgsgsdg', 2, '2019-12-22 17:32:32', NULL),
+(1, 'Dinum', 'Dissnayaka', 'Mahawela', 'm.dinum@gmail.com', 770307283, '325/3 gona', 'dgsgsdg', 2, '2019-12-22 17:32:32', '2019-12-23 15:58:36'),
 (2, 'Dinum', 'Dissnayaka', 'Mahawela', 'dinum@gmail.com', 770307283, 'fsaffasfasfasf', 'dgsgsdg', 2, '2019-12-22 17:35:52', NULL),
-(3, 'Dinum', 'Dissnayaka', 'Mahawela', 'dinum@gmail.com', 770307283, 'fsaffasfasfasf', 'cvbdcfhndf', 1, '2019-12-22 17:45:29', NULL);
+(3, 'Dinum', 'Dissnayaka', 'Mahawela', 'dinum@gmail.com', 770307283, 'fsaffasfasfasf', 'cvbdcfhndf', 1, '2019-12-22 17:45:29', '2019-12-23 15:23:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user_vacancy`
+--
+
+DROP TABLE IF EXISTS `tbl_user_vacancy`;
+CREATE TABLE IF NOT EXISTS `tbl_user_vacancy` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `vacancy_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_vacancies`
+--
+
+DROP TABLE IF EXISTS `tbl_vacancies`;
+CREATE TABLE IF NOT EXISTS `tbl_vacancies` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `tittle` varchar(500) NOT NULL,
+  `summery` text NOT NULL,
+  `type` tinyint(1) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `location` varchar(500) DEFAULT NULL,
+  `pay_type` tinyint(1) NOT NULL,
+  `payment` double DEFAULT NULL,
+  `description` text,
+  `added_date` datetime NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_vacancies`
+--
+
+INSERT INTO `tbl_vacancies` (`id`, `tittle`, `summery`, `type`, `start`, `end`, `location`, `pay_type`, `payment`, `description`, `added_date`, `status`) VALUES
+(1, 'testing data', 'vxdgs', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'gretgyer', 2, 36346, 'dfghdfhdf', '2019-12-23 18:25:27', 2),
+(2, 'testing data 2', 'sdgfsdgsdg', 2, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'sdgsdhgsdhgsdhg', 2, 463463, 'dfhdfhdf', '2019-12-23 18:31:49', 2),
+(3, 'testing data 3', 'dsgsdgsdgsdg', 2, '2019-12-24 00:03:00', '2019-12-28 00:03:00', 'sdgsdgsdg', 2, 3456346, 'sdgbsdgds', '2019-12-23 18:34:01', 2);
 
 -- --------------------------------------------------------
 
@@ -271,7 +349,7 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
   PRIMARY KEY (`id`),
   KEY `user_idx` (`user_id`),
   KEY `role_idx` (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_roles`
@@ -281,7 +359,8 @@ INSERT INTO `user_roles` (`id`, `user_id`, `role_id`) VALUES
 (1, 1, 1),
 (2, 2, 2),
 (3, 3, 3),
-(4, 4, 2);
+(4, 4, 2),
+(5, 5, 4);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
